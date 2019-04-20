8fb0385b43673a7a800e205a8f795269ba55f718
beta-scanner-6136 <mahdi.saeikhalili@gmail.com>
2019-04-20 02:36:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo St
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 theBeatlesPlay(musicians, instruments){
   result = []
-  for(i=0;i<4;i++){
-    result.push(``)
+  for(i=0;i<musicians.length;i++){
+    result.push(`${musicians[i]} plays ${instruments[i]}`)
   }
+  return result
 }

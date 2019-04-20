550c258a083eaee695dbb49025aefa2d39c04190
beta-scanner-6136 <mahdi.saeikhalili@gmail.com>
2019-04-20 02:48:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 function theBeatlesPlay(musicians, instruments){
   var result = []
-  for(let i=0;i<4;i++){
+  for(var i=0;i<4;i++){
     result.push(`${musicians[i]} plays ${instruments[i]}`)
   }
   return result

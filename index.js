90631233b5904d7bfc91f7456f8bf422912f8c41
beta-scanner-6136 <mahdi.saeikhalili@gmail.com>
2019-04-20 02:37:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // add solution here
 
-theBeatlesPlay(musicians, instruments){
+function theBeatlesPlay(musicians, instruments){
   result = []
   for(i=0;i<musicians.length;i++){
     result.push(`${musicians[i]} plays ${instruments[i]}`)

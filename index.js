287f83e1e69382fb3f1ddb475365d9863e5f05c7
beta-scanner-6136 <mahdi.saeikhalili@gmail.com>
2019-04-20 02:55:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,5 +9,10 @@ function theBeatlesPlay(musicians, instruments){
 }
 
 function johnLennonFacts(facts){
-  
+  var i = 0
+  while(i<facts.length){
+    facts[i] += "!!!"
+    i++
+  }
+  return facts
 }
